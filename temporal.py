6a692ed78c8b88ab936e20bd49c486204721817d
wxn <pkuwxn@foxmail.com>
2015-12-08 20:20:00 +0800

    Census the average registrant count at some checkpoints.

--- temporal.py
+++ temporal.py
@@ -1,7 +1,7 @@
 
 from pymongo import MongoClient
 from datetime import datetime
-from gen_data import cmp_datetime
+from cf import cmp_datetime
 
 
 client = MongoClient()
@@ -17,6 +17,7 @@ def census(date_from, minute):
 
     num_ratios = 0
     ratio_sum = 0.0
+    regs_sum = 0
 
     for challenge in db.challenges.find(condition):
         if u"registrants" not in challenge:
@@ -26,6 +27,9 @@ def census(date_from, minute):
         count = 0
 
         regs = challenge[u"registrants"]
+        if len(regs) == 0:
+            continue
+
         regs.sort(cmp=lambda x, y: cmp_datetime(x[u"registrationDate"],
                                                 y[u"registrationDate"]))
 
@@ -37,8 +41,12 @@ def census(date_from, minute):
 
         num_ratios += 1
         ratio_sum += float(count) / len(regs)
+        regs_sum += count
 
-    return ratio_sum / num_ratios if num_ratios > 0 else 0.0
+    if num_ratios > 0:
+        return ratio_sum / num_ratios, int(round(regs_sum / num_ratios))
+    else:
+        return 0.0, 0
 
 
 def main():
@@ -51,24 +59,24 @@ def main():
     # 4096 minutes = 2.85 days
     # 8192 minutes = 5.69 days
 
-    # 1 minutes: 0.77%
-    # 2 minutes: 0.89%
-    # 4 minutes: 1.27%
-    # 8 minutes: 2.09%
-    # 16 minutes: 3.63%
-    # 32 minutes: 6.14%
-    # 64 minutes: 10.46%
-    # 128 minutes: 17.60%
-    # 256 minutes: 27.31%
-    # 512 minutes: 40.55%
-    # 1024 minutes: 58.21%
-    # 2048 minutes: 75.62%
-    # 4096 minutes: 89.75%
-    # 8192 minutes: 96.50%
+    # 1 minutes: 0.72% - 0
+    # 2 minutes: 0.84% - 0
+    # 4 minutes: 1.20% - 0
+    # 8 minutes: 1.98% - 0
+    # 16 minutes: 3.48% - 0
+    # 32 minutes: 6.00% - 0
+    # 64 minutes: 10.21% - 1
+    # 128 minutes: 17.29% - 2
+    # 256 minutes: 26.90% - 4
+    # 512 minutes: 40.07% - 7
+    # 1024 minutes: 57.74% - 10
+    # 2048 minutes: 75.16% - 14
+    # 4096 minutes: 89.52% - 18
+    # 8192 minutes: 96.42% - 20
 
     while coverage < 0.9:
-        coverage = census(date_from, minute)
-        print "%d minutes: %.2f%%" % (minute, coverage * 100)
+        coverage, regs_mean = census(date_from, minute)
+        print "%d minutes: %.2f%% - %d" % (minute, coverage * 100, regs_mean)
 
         minute *= 2
 

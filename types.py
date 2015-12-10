64437c6595f5480509e91c877294476e9d51b240
wxn <xiaoning.wang@intel.com>
2015-12-10 22:03:56 +0800

    Add challenge types statistics.

---
+++ types.py
@@ -0,0 +1,43 @@
+
+from pymongo import MongoClient
+
+from datetime import datetime
+from collections import defaultdict
+
+
+client = MongoClient()
+db = client.topcoder
+
+
+def census(date_from):
+    condition = {
+        u"postingDate": {
+            u"$gte": date_from,
+        }
+    }
+
+    numChallenges = 0
+    types = defaultdict(int)
+    challengeTypes = defaultdict(int)
+
+    for challenge in db.challenges.find(condition):
+        numChallenges += 1
+        types[challenge[u"type"]] += 1
+        challengeTypes[challenge[u"challengeType"]] += 1
+
+    print "Total:", numChallenges
+
+    print ""
+    print "Type distribution:"
+    for t in types:
+        print "  ", t, types[t]
+
+    print ""
+    print "challengeType distribution:"
+    for ct in challengeTypes:
+        print "  ", ct, challengeTypes[ct]
+
+
+if __name__ == "__main__":
+    date_from = datetime(2015, 1, 1)
+    census(date_from)

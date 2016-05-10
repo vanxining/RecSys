3a50c530f6409f8e23c25c591a957d025ad3b70c
wxn <vanxining@gmail.com>
2016-05-10 14:54:17 +0800

    Add a statistics script to count the platforms, technologies and winners.

---
+++ stats.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+
+import pymongo
+from collections import defaultdict
+
+
+client = pymongo.MongoClient()
+db = client.topcoder
+
+
+def platforms_and_technologies():
+    count = defaultdict(int)
+
+    for challenge in db.challenges.find():
+        for plat in challenge["platforms"]:
+            count[plat] += 1
+
+        for tech in challenge["technology"]:
+            count[tech] += 1
+
+    for key in count:
+        print "%s: %s" % (key, count[key])
+
+    print "\nTotal:", len(count), "\n"
+
+
+def winners():
+    count = defaultdict(int)
+
+    for challenge in db.challenges.find():
+        winner_found = False
+
+        for submission in challenge[u"finalSubmissions"]:
+            if submission[u"placement"] == 1:
+                if submission[u"submissionStatus"] == u"Active":
+                    if winner_found:
+                        print challenge
+                        assert False
+
+                    count[submission[u"handle"]] += 1
+                    winner_found = True
+
+    for key in count:
+        print "%s: %s" % (key, count[key])
+
+    print "\nTotal:", len(count), "\n"
+
+
+if __name__ == "__main__":
+    platforms_and_technologies()
+    winners()

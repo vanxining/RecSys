cea5770149ed8eb6ddbf37fb7c2e1b111f2239c7
wxn <pkuwxn@foxmail.com>
2016-07-11 23:17:32 +0800

    Introduce dimension reduction for data generation.

--- data.py
+++ data.py
@@ -8,6 +8,8 @@ from collections import defaultdict
 import pymongo
 import numpy as np
 
+import ptcat
+
 
 def cmp_datetime(a, b):
     return -1 if a < b else 1 if a > b else 0
@@ -45,7 +47,7 @@ class Data(object):
         # TODO: challengeType
         return (challenge[u"challengeType"] == u"First2Finish" and
                 challenge[u"type"] == u"develop" and
-                len(challenge["prize"]) > 0)
+                len(challenge[u"prize"]) > 0)
 
     def training_set(self):
         condition = {
@@ -80,10 +82,23 @@ def get_winner(challenge):
                 return submission[u"handle"]
 
 
-class DlData(Data):
-    # First prize, Prize count
-    NUM_VITAL_FEATURES = 2
+def _calc_duration(challenge, keyword):
+    posting_date = challenge["postingDate"]
+    end_date = challenge[keyword]
+    delta = end_date - posting_date
 
+    return delta.days
+
+
+def _calc_max_working_days(challenge):
+    return _calc_duration(challenge, "submissionEndDate")
+
+
+def _calc_appeals_duration(challenge):
+    return _calc_duration(challenge, "appealsEndDate")
+
+
+class DlData(Data):
     def __init__(self):
         Data.__init__(self)
 
@@ -97,24 +112,23 @@ class DlData(Data):
                 self.user_ids[user] = index
                 index += 1
 
-        self.plat_tech = defaultdict(int)
         self.nb_training = 0
         self.nb_test = 0
 
         for challenge in Data.training_set(self):
             self.nb_training += 1
-            self.count_platforms_and_technologies(challenge)
 
         for challenge in Data.test_set(self):
             self.nb_test += 1
-            self.count_platforms_and_technologies(challenge)
-
-        for index, pt in enumerate(self.plat_tech):
-            self.plat_tech[pt] = index
 
         print "# traning:", self.nb_training
         print "# test:", self.nb_test
-        print "# plaforms & technologies:", len(self.plat_tech)
+
+    def _count_vital_features(self):
+        fake_line = {}
+        DlData._fill_line(fake_line, self.db.challenges.find()[0])
+
+        return len(fake_line)
 
     def count_user_win_times(self):
         user_win_times = defaultdict(int)
@@ -126,13 +140,6 @@ class DlData(Data):
 
         print "Max win times:", max(user_win_times.values())
 
-    def count_platforms_and_technologies(self, challenge):
-        for plat in challenge[u"platforms"]:
-            self.plat_tech[plat] += 1
-
-        for tech in challenge[u"technology"]:
-            self.plat_tech[tech] += 1
-
     def is_challenge_ok(self, challenge):
         ok = Data.is_challenge_ok(self, challenge)
 
@@ -143,16 +150,22 @@ class DlData(Data):
             return ok
 
     def validate_matrix(self, m, iterator):
+        nb_vital_features = self._count_vital_features()
+
+        def ptcat_index(platech):
+            return nb_vital_features + ptcat.get_category(platech) - 1
+
         count = 20
+
         for index, challenge in enumerate(iterator(self)):
             if index % 5 != 0:
                 continue
 
             for plat in challenge[u"platforms"]:
-                assert m[index, self.plat_tech[plat] + self.NUM_VITAL_FEATURES] == 1
+                assert m[index, ptcat_index(plat)] == 1
 
             for tech in challenge[u"technology"]:
-                assert m[index, self.plat_tech[tech] + self.NUM_VITAL_FEATURES] == 1
+                assert m[index, ptcat_index(tech)] == 1
 
             winner = get_winner(challenge)
 
@@ -163,21 +176,61 @@ class DlData(Data):
             if count == 0:
                 break
 
+    @staticmethod
+    def _fill_line(line, challenge):
+        index = 0
+
+        # First prize
+        line[index] = challenge[u"prize"][0]
+        index += 1
+
+        # Number of prize
+        line[index] = len(challenge[u"prize"])
+        index += 1
+
+        if False:
+            # TODO: Review type
+            line[index] = 1
+            index += 1
+
+        # Max working days
+        line[index] = _calc_max_working_days(challenge)
+        index += 1
+
+        if False:
+            # TODO: Type (develop, ...)
+            line[index] = 1
+            index += 1
+
+            # TODO: Challenge type (F2F, ...)
+            line[index] = 1
+            index += 1
+
+        # Appeals duration in days
+        line[index] = _calc_appeals_duration(challenge)
+        index += 1
+
     def generate_matrix(self, nb_rows, iterator):
-        nb_cols = self.NUM_VITAL_FEATURES + len(self.plat_tech) + 1
+        nb_vital_features = self._count_vital_features()
+        nb_platech = ptcat.get_number_of_platech()
+        nb_cols = nb_vital_features + nb_platech + 1
+
+        def ptcat_index(platech):
+            return nb_vital_features + ptcat.get_category(platech) - 1
+
         m = np.zeros((nb_rows, nb_cols), dtype=np.uint16)
 
         for index, challenge in enumerate(iterator(self)):
-            m[index, 0] = challenge[u"prize"][0]
-            m[index, 1] = len(challenge[u"prize"])
+            line = m[index]
+            DlData._fill_line(line, challenge)
 
             for plat in challenge[u"platforms"]:
-                m[index, self.plat_tech[plat] + self.NUM_VITAL_FEATURES] = 1
+                line[ptcat_index(plat)] = 1
 
             for tech in challenge[u"technology"]:
-                m[index, self.plat_tech[tech] + self.NUM_VITAL_FEATURES] = 1
+                line[ptcat_index(tech)] = 1
 
-            m[index, nb_cols - 1] = self.user_ids[get_winner(challenge)]
+            line[nb_cols - 1] = self.user_ids[get_winner(challenge)]
 
         return m
 

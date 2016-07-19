d9f22c9ebe4ddcbc8714b13f71710d4059ae56fa
wxn <pkuwxn@foxmail.com>
2016-07-20 00:38:14 +0800

    Do not read the .pyc binary file of the config files incorrectly.

--- config/classifiers.sample.py
+++ config/classifiers.sample.py
@@ -1,11 +1,17 @@
 
-raw = open(__file__).read().strip()
+path = __file__
+if path[-1] == 'c':
+    path = path[:-1]
+
+raw = open(path).read().strip()
 raw = raw[raw.index("topn" + " "):]
 
 # "topn" **MUST** be the first attribute
 
 topn = 30
 normalize_dataset = False
+
+# NB, LR, MLP
 classifier = "LR"
 
 if classifier == "MLP":

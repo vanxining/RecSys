df125a6cbb3088152e6a4ccb3fbae49ca7bf368e
wxn <vanxining@gmail.com>
2017-01-12 00:05:49 +0800

    Add more options.

--- find_one.py
+++ find_one.py
@@ -1,9 +1,40 @@
+import argparse
+
 from bson import json_util
 import pymongo
 
 
 client = pymongo.MongoClient()
-db = client.freelancer
 
-row = db.projects.find_one({"result.bids": {"$size": 10}})
-print json_util.dumps(row)
+
+def topcoder():
+    db = client.topcoder
+    row = db.challenges.find_one()
+
+    return json_util.dumps(row)
+
+
+def topcoder_user():
+    db = client.topcoder
+    row = db.users.find_one({"handle": "vvvpig"})
+
+    return json_util.dumps(row)
+
+
+def freelancer():
+    db = client.freelancer
+    row = db.projects.find_one({"result.bids": {"$size": 10}})
+
+    return json_util.dumps(row)
+
+
+def main():
+    parser = argparse.ArgumentParser("find_one")
+    parser.add_argument("dataset", help="the dataset to extract")
+
+    args = parser.parse_args()
+    print(globals()[args.dataset]())
+
+
+if __name__ == "__main__":
+    main()

58e8cb3352d3b2cc4419b22c64c6c8113a82c16e
wxn <vanxining@gmail.com>
2017-01-15 14:55:50 +0800

    Reword sample config file.

--- config/data_freelancer.sample.py
+++ config/data_freelancer.sample.py
@@ -4,14 +4,18 @@ from time import mktime
 import util
 
 
+def _T(*t):
+    return mktime(datetime(*t).timetuple())
+
+
 raw = util.get_py_config_file_content(__file__)
 
 # "_config" **MUST** be the first attribute
 
 _config = "data_freelancer"
 
-training_set_begin_date = mktime(datetime(2016, 4, 1).timetuple())
-training_set_end_date = mktime(datetime(2016, 8, 15).timetuple())
+training_set_begin_date = _T(2016, 4, 1)
+training_set_end_date = _T(2016, 8, 15)
 
 training_set_limit = 3000
 test_set_limit = 300
